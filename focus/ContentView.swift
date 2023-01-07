c6e2b892e96dad1f5399fefe3ce172177823338c
Ghadahss <Ghadah.s.alzahrani@gmail.com>
2023-01-07 22:18:01 +0300

    Initial Commit

---
+++ focus/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  focus
+//
+//  Created by Ghadah on 14/06/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ focus/focusApp.swift
@@ -0,0 +1,17 @@
+//
+//  focusApp.swift
+//  focus
+//
+//  Created by Ghadah on 14/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct focusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
